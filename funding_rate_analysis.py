0b18ece3ee5dc0b61c94faeb02e35abb9cdfd154
jie <jiejiezhangsta@gmail.com>
2025-12-31 18:11:02 +0800

    updates on 'poetry add common'

--- funding_rate_analysis.py
+++ funding_rate_analysis.py
@@ -1,4 +1,4 @@
-############### Create python virtual environment as https://github.com/stajie99/funding-rate-arb-analysis/tree/main
+############### Create python virtual environment Method 1: as https://github.com/stajie99/funding-rate-arb-analysis/tree/main
 
 # pip install poetry
 # # Verify poetry installation
@@ -13,8 +13,25 @@
 
 # # # Create virtual environment and install dependencies
 # # poetry install
+# # # After poetry install, Poetry creates the virtual environment in a system-specific location, 
+# not in your project folder by default. Here's how to find and manage it.
 
-################ do not run below
+# 1. Show all poetry virtual environments
+poetry env list
+
+# 2. Show info about current environment
+poetry env info
+
+# 3. Get the full path
+poetry env info --path
+
+# Configure Poetry to create venv in project
+poetry config virtualenvs.in-project true
+
+# Now when you run poetry install, it creates .venv/ in your project
+poetry install
+
+################ do not run below: Method 2
 # # Create virtual environment
 # python -m venv venv
 
