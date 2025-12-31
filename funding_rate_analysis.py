0dead3563f1b6d6f8baec0caa8410ca6f3cdbee6
jie <jiejiezhangsta@gmail.com>
2025-12-31 17:06:01 +0800

    virtual environment 'venv' created

--- funding_rate_analysis.py
+++ funding_rate_analysis.py
@@ -1,2 +1,22 @@
-# pip install poetry
+pip install poetry
+# Verify poetry installation
+poetry --version
 # Poetry helps you declare, manage and install dependencies of Python projects, ensuring you have the right stack everywhere.
+
+# # Configure poetry to create virtual environments in your project folder
+# poetry config virtualenvs.in-project true
+
+# # Initialize poetry
+# poetry init
+
+# # Create virtual environment and install dependencies
+# poetry install
+
+# Create virtual environment
+python -m venv venv
+
+# Activate it
+venv\Scripts\activate.bat
+
+# To deactivate
+deactivate
